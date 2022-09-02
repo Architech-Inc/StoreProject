770509d9f147d37eff9ca5bc8ffc1dc9fac7bc9f
Alain Kimbu <54691684+Rodern@users.noreply.github.com>
2022-09-02 09:32:15 +0100

    Fix: cors policy

--- StoreAPI/Program.cs
+++ StoreAPI/Program.cs
@@ -18,7 +18,10 @@ builder.Services.AddScoped<ICategoryService, CategoryService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddCors(p => p.AddPolicy("corspolicy", build =>
+{
+	build.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -27,7 +30,7 @@ if (app.Environment.IsDevelopment())
 	app.UseSwagger();
 	app.UseSwaggerUI();
 }
-
+app.UseCors("corspolicy");
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
